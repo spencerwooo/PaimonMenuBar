8d63a89758a7ef02d91d4c35777037da2e2f605b
spencerwooo <spencer.woo@outlook.com>
2022-03-23 11:57:08 +0800

    initial project setup

---
+++ PaimonMenuBar/PaimonMenuBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaimonMenuBarApp.swift
+//  PaimonMenuBar
+//
+//  Created by Spencer Woo on 2022/3/23.
+//
+
+import SwiftUI
+
+@main
+struct PaimonMenuBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
