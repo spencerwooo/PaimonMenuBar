217dce10010e4f67efc91b7fdaf8595031ebedd6
Wish <breezewish@outlook.com>
2022-04-30 19:33:23 +0800

    grumble: GameRecordRenderer doesn't need to be ObservableObject any more

--- PaimonMenuBar/GameRecordRenderer.swift
+++ PaimonMenuBar/GameRecordRenderer.swift
@@ -11,7 +11,7 @@ import SwiftUI
 import Combine
 import Defaults
 
-class GameRecordRenderer: ObservableObject {
+class GameRecordRenderer {
     static let shared = GameRecordRenderer()
     
     /**
